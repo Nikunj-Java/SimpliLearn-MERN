9eba72412e2af4af1fce189203bbe6ecec202b82
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-09-21 19:48:01 +0530

    updated

---
+++ Phase-2/Session-1/firstapp1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/helloworld';
+import Greeting from './components/greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld/>
+      <Greeting/>
+    </div>
+  );
+}
+
+export default App;
