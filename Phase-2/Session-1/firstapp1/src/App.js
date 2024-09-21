2d7a250b1db0231294383b5696b61b7a0c516726
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-09-21 21:55:29 +0530

    JSX Examples,UseState

--- Phase-2/Session-1/firstapp1/src/App.js
+++ Phase-2/Session-1/firstapp1/src/App.js
@@ -5,15 +5,31 @@ import Greeting from './components/greeting';
 import StyleComponent from './components/stylecomponent';
 import ActionButton from './components/actionbutton';
 import NumberList from './components/numberlist';
+import Counter from './components/counter';
+import Toggle from './components/toggle';
+import Form from './components/form';
+import UserInfo from './components/userinfo';
+import ArrayState from './components/arraysstate';
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld/>
+      {/* <HelloWorld/>
       <Greeting/>
       <StyleComponent/>
       <ActionButton/>
       <NumberList/>
+      <Counter/>
+      <Toggle/> 
+       <Form/>
+       <UserInfo/>*/}
+
+       <ArrayState/>
+
+       
+
+     
+
     </div>
   );
 }
