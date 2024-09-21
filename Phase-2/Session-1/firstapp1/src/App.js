3cdf8a17feae347cc0bfd1ec3a7a1347c5e71657
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-09-21 20:13:37 +0530

    JSX Examples

--- Phase-2/Session-1/firstapp1/src/App.js
+++ Phase-2/Session-1/firstapp1/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/helloworld';
 import Greeting from './components/greeting';
+import StyleComponent from './components/stylecomponent';
+import ActionButton from './components/actionbutton';
+import NumberList from './components/numberlist';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
       <Greeting/>
+      <StyleComponent/>
+      <ActionButton/>
+      <NumberList/>
     </div>
   );
 }
