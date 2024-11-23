f20b34255049d61f983178920f66d0d7ad935af0
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-11-23 21:33:49 +0530

    QUEUE,Tree Traversal-examples

--- Phase-3/Session-3/02.BST.js
+++ Phase-3/Session-3/02.BST.js
@@ -19,13 +19,30 @@ class  BinaryTree{
     //in-Order Traversal
     inorderTraversal(node){
         if(node){
-            this.inorderTraversal(node.left);
+            this.inorderTraversal(node.left);  //left
             console.log("Visited Root node");
-            console.log(node.data);
-            this.inorderTraversal(node.right);
+            console.log(node.data);            //root node
+            this.inorderTraversal(node.right); //right node
         }
+    }
+    //pre-order traversal
+    preOrderTraversal(node){
+        if(node){
+            console.log(node.data);          //root node
+            console.log("Visited Root node");
+            this.preOrderTraversal(node.left); //left node
+            this.preOrderTraversal(node.right);//right node
+        }
+    }
 
-
+    //post  order traversal
+    postOrderTraversal(node){
+        if(node){
+            this.postOrderTraversal(node.left);
+            this.postOrderTraversal(node.right);
+            console.log(node.data);
+            console.log("Visited Root Node");
+        }
     }
 
 }
@@ -44,3 +61,15 @@ tree.root.left.right=new Node(5);
 console.log("In order traversal");
 
 tree.inorderTraversal(tree.root);
+
+
+                             //1
+                //           /  \
+                //          2     3
+                //         /  \
+                //        4    5
+console.log("Pre-Order Traversal");
+tree.preOrderTraversal(tree.root);
+
+console.log("Post-order Traversal");
+tree.postOrderTraversal(tree.root);
