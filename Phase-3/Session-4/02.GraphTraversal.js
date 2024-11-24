f1750fb9e1cff13657a37dacb549f014d1abb8cc
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-11-24 21:53:30 +0530

    Graph,DFS Traversal,BFS Traversal

--- Phase-3/Session-4/02.GraphTraversal.js
+++ Phase-3/Session-4/02.GraphTraversal.js
@@ -35,6 +35,39 @@ class Graph{
         //call recursive helper function to print DFS traversal
         this.DFSutil(v,visited);
     }
+
+    BFS(s){
+        let visited= new Array(this.V);
+        for(let i=0;i<this.V;i++)
+            visited[i]=false;
+
+        //create a queue for BFS
+        let queue=[];
+
+        //Mark all current node as visited and enqueue it
+
+        visited[s]=true;
+        queue.push(s);
+
+        while(queue.length>0){
+            //dequeue a vertex from queue and print it
+            s=queue[0];
+            console.log(s+" ");
+            queue.shift();
+
+            //Get all the adjacent vertices of the dequeue ,say vertex s
+            //if a adjacent has not been visited ,
+            //then we will mark it as enqueue it
+
+            this.adj[s].forEach((adjacent,i)=>{
+                if(!visited[adjacent]){
+                    visited[adjacent]=true;
+                    queue.push(adjacent);
+                }
+            });
+        }
+
+    }
 }
 
 
@@ -49,12 +82,14 @@ g.addEdge(2,4);
 g.addEdge(4,5);
 g.addEdge(4,6);
 
-console.log("DFS starting from vertex 0: ");
+console.log("DFS starting from vertex 0 : ");
 g.DFS(0);
 
 console.log("DFS starting from vertex 1: ");
 g.DFS(1);
 
-console.log("DFS starting from vertex 4: ");
-g.DFS(4);
- 
+console.log("BFS starting from vertex 0 :");
+g.BFS(0);
+
+console.log("BFS starting from vertex 2 :");
+g.BFS(2);
