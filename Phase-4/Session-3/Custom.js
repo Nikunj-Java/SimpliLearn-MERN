087fca644fbe8ba07ea8f0f0dc7d57e0775145f6
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-01-18 19:51:30 +0530

    Node js Notes

---
+++ Phase-4/Session-3/Custom.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Node.js Developement");
+
+function hello(){
+    console.log("Hello World");
+}
+
+hello();
+
+const array=[1,2,3,4,5,6];
+
+array.forEach((value)=>{
+    console.log(value);
+})
