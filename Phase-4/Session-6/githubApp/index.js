54f6e17a23bdfb862927ef402348ce19728dc3ef
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-01-26 22:37:22 +0530

    Open AI API,Open Whether API

--- Phase-4/Session-6/githubApp/index.js
+++ Phase-4/Session-6/githubApp/index.js
@@ -9,4 +9,18 @@ async function getUser(username){
     }
 }
 
-getUser('Nikunj-Java');
+//whether and forecast api
+//api_key: 19043c99a93215d99870aa62ecc42df4
+
+async function getWhetherData(city){
+    const API_key='Your_API_Key';
+    try {
+        const response=await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//getUser('Nikunj-Java');
+getWhetherData('China');
