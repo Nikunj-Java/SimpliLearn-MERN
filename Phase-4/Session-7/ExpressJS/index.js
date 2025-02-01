c7df11fe07a193a82ad24bb83b5fa03392d19456
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-02-01 20:18:03 +0530

    Express.js,Routing Mechanism

--- Phase-4/Session-7/ExpressJS/index.js
+++ Phase-4/Session-7/ExpressJS/index.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
 const port=3000;
-app.get('/',(req,res)=>{
-    res.send('Hello Server!.\n Welcome to Express.js') 
-});
+
+app.route('/router')
+.get((req,res,next)=>{
+     console.log('Get Method Called');
+     res.send({message:'Get Method Callled'});
+})
+.post((req,res,next)=>{
+    console.log('Post Method Called');
+     res.send({message:'Post Method Callled'});
+})
 
 app.listen(port,()=>{
     console.log(`Express App is Running and LIVE on Port: ${port}`);
