1ea6e369b727b16a0432e039c42b7eb06003862e
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-02-09 21:48:33 +0530

    Pagination

--- Phase-4/Session-10/routes.js
+++ Phase-4/Session-10/routes.js
@@ -3,10 +3,23 @@ const express=require('express');
 const ProductModel=require('./model');
 const router=express.Router();
 //pagination
-//localhost:5000/api/products?page=3&limit=3
+//localhost:4000/api/product?page=3&limit=3
 
 //WAP to get all products here
-
+router.get("/",async(req,res,next)=>{
+    //reading query parameters for pagination
+    const page=parseInt(req.query.page) || 1;
+    const limit=parseInt(req.query.limit) || 10
+    try {
+        const skip=(page-1)*limit;
+        const products=await ProductModel.find().skip(skip).limit(limit);
+        const totalProducts= await ProductModel.countDocuments();
+        res.status(200).send({products,currentPage:page,totalPages:Math.ceil(totalProducts/limit)})
+    } catch (error) {
+        next(error);
+    }
+})
+ 
 
 
 //create Soms Data
