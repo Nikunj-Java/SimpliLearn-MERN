143c104d9affa8aad295e6b2701729c1774ebc55
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-02-16 20:33:53 +0530

    Mocha Testing

--- Phase-4/Session-11/api-testing1/test/api.test.js
+++ Phase-4/Session-11/api-testing1/test/api.test.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { expect } from 'chai';
 
-const baseUrl = 'https://jsonplaceholder.typicode.com'; // Sample API
+const baseUrl = 'https://jsonplaceholder.typicode.com';  
 
 describe('API Testing with Mocha (ES Modules)', function () {
     it('should return a list of users', async function () {
@@ -9,6 +9,16 @@ describe('API Testing with Mocha (ES Modules)', function () {
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('array');
     });
+
+    it('should create a new post',async function(){
+        const res= await request(baseUrl)
+        .post('/users')
+        .send({'id': 20,'name': 'Nikunj Soni','username': 'nik_unj','email': 'nikunj@testmail.com'})
+        .set('Content-Type','application/json')
+
+        expect(res.status).to.equal(201);
+        expect(res.body).to.include({'name': 'Nikunj Soni','username': 'nik_unj','email': 'nikunj@testmail.com'})
+    })
 
     
     //install dev dependecies like
