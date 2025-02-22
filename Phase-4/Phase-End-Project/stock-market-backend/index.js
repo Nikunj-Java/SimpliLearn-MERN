d61b910162bb2fd879dd167168c25148151684f0
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-02-22 20:40:50 +0530

    Phase End Project

--- Phase-4/Phase-End-Project/stock-market-backend/index.js
+++ Phase-4/Phase-End-Project/stock-market-backend/index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import axios from 'axios'
 
 dotenv.config();
 
 const app=express();
 const PORT= process.env.PORT || 5000;
+const FINNHUB_API_KEY=process.env.FINNHUB_API_KEY;
 
 app.use(cors());
 app.use(express.json());
 
+const FINNHUB_BASE_URL="https://finnhub.io/api/v1";
 
+/*
 let stocks=[
     {id:1, name:'Apple', symbol:'AAPL',price:175},
     {id:2, name:'Tesla', symbol:'TSLA',price:175},
@@ -27,8 +31,26 @@ app.get('/stocks/:id',(req,res)=>{
     const stock=stocks.find(s=>s.id===parseInt(req.params.id));
     if(!stock) return res.status(404).json({message:'Stock Not Found'});
     res.send(stock);
+});
+*/
+
+
+app.get('/stocks/all',async(req,res)=>{
+    const exchange=req.query.exchange || 'US' //default value
+
+    try {
+        const response=await axios.get(`${FINNHUB_BASE_URL}/stock/symbol`,{
+            params:{ exchange, token:FINNHUB_API_KEY }, //mendatory
+            headers:{'X-Finnhub-Token':FINNHUB_API_KEY }//mendatory
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({message:'Failed to fetch stocks list',details:error.response?.data || error.message});
+    }
 })
 
+//create the same
+//create an api to get stock by id
 
 app.listen(PORT,()=>{
     console.log(`Server is running and Up! on PORT: ${PORT}`)
