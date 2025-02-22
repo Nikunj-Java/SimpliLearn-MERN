304cd6d8b5adf61dacaabe4aed21f18c59ccc269
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-02-22 19:44:03 +0530

    Phase End Project

---
+++ Phase-4/Phase-End-Project/stock-market-backend/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app=express();
+const PORT= process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+let stocks=[
+    {id:1, name:'Apple', symbol:'AAPL',price:175},
+    {id:2, name:'Tesla', symbol:'TSLA',price:175},
+    {id:3, name:'Google', symbol:'GOGL',price:175},
+    {id:4, name:'Amazon', symbol:'AMZN',price:175}
+];
+
+
+app.get('/stocks',(req,res)=>{
+    res.json(stocks);
+});
+
+app.get('/stocks/:id',(req,res)=>{
+    const stock=stocks.find(s=>s.id===parseInt(req.params.id));
+    if(!stock) return res.status(404).json({message:'Stock Not Found'});
+    res.send(stock);
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running and Up! on PORT: ${PORT}`)
+});
