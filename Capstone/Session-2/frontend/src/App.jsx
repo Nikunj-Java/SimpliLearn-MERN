8de0333086322c5890267728aa85b7b1038e4d22
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-03-09 19:40:58 +0530

    Frontend

---
+++ Capstone/Session-2/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import UserForm from './components/userform'
+
+function App() {
+   
+
+  return (
+    <>
+      <div  className='container'>
+        <UserForm/>
+        </div>
+    </>
+  )
+}
+
+export default App
